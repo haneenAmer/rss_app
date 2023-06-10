3922c5bac456d29b9b766d718c248dfad55cddc4
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-06-10 20:29:52 +0300

    create splash screen

---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'navigation_bar.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  /// creat new timer
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 5), timerofsplash);
+  }
+
+  ///creat class to move to the home page
+  void timerofsplash() {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => NavBar()),
+      (Route<dynamic> route) => false,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: AnimatedSplashScreen(
+          backgroundColor: Color(0xff1B1B1D),
+          splash: 'images/MiCinema-logo.png',
+          nextScreen: NavBar(),
+          splashTransition: SplashTransition.scaleTransition,
+        ),
+      ),
+    );
+  }
+}
