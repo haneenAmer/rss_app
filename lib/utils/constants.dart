180992d6db576bc0b7323886376b4426d16fbce0
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-06-10 20:30:28 +0300

    create AppColors class and app TextSyle

---
+++ lib/utils/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppColors {
+  Color greenMain = Color(0xffBEF22E);
+  Color greyBackground = const Color(0xffEFF3F6);
+  Color whiteMain = Color(0xffFFFFFF);
+  Color blackMain = Color(0xff000000);
+  Color greyBorder = Color(0xffA0AEC0);
+}
+
+const TextStyle OptionStyle = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w600,
+  fontFamily: 'Tajwal',
+);
