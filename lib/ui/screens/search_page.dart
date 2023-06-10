d746db596af4596602ed2c26e5621f8e64492ce1
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-06-10 20:29:33 +0300

    creat search screen

---
+++ lib/ui/screens/search_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SeachScreen extends StatefulWidget {
+  const SeachScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SeachScreen> createState() => _SeachScreenState();
+}
+
+class _SeachScreenState extends State<SeachScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
