91ec7647057696061a92db0b6743e820b4eb4e27
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-06-14 21:54:21 +0300

    homescreen edite

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:rrs_app/utils/constants.dart';
 
@@ -16,11 +18,11 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: AppColors.greyBackground,
       appBar: AppBar(
         actions: [
-          Text('مستكشف المطاعم', style: OptionStyle),
+          const Text('مستكشف المطاعم', style: OptionStyle),
         ],
-        leading: Padding(
-          padding: const EdgeInsets.only(bottom: 30.0),
-          child: Icon(
+        leading: const Padding(
+          padding: EdgeInsets.only(bottom: 12.0),
+          child: const Icon(
             Icons.person,
             color: AppColors.blackMain,
             size: 30.0,
@@ -31,42 +33,91 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Column(children: [
-          Container(
-            width: double.infinity,
-            height: 200,
-            decoration: BoxDecoration(
-              //image: DecorationImage(
-              //image: AssetImage('assets/images/ads.jpg'),
-              // fit: BoxFit.fill,
-              // ),
-              color: Colors.amber,
-              borderRadius: BorderRadius.circular(12),
-            ),
-          ),
-          Container(
-              //margin: const EdgeInsets.symmetric(vertical: 8.0),
-              //padding: EdgeInsets.only(left: 12),
-              height: 100,
+        child: ListView(scrollDirection: Axis.vertical, children: [
+          Column(children: [
+            Container(
               width: double.infinity,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.all(8),
-                  itemCount: 6,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      margin: EdgeInsets.all(5),
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(4),
-                        // image: DecorationImage(
-                        //  image: AssetImage('assts/images/ads.jpg'))
-                      ),
-                    );
-                    VerticalSpace(12);
-                  })),
+              height: 200,
+              decoration: BoxDecoration(
+                //image: DecorationImage(
+                //image: AssetImage('assets/images/ads.jpg'),
+                // fit: BoxFit.fill,
+                // ),
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+            SizedBox(
+                height: 150,
+                width: double.infinity,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.all(8),
+                    itemCount: 6,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.all(5),
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(4),
+                              // image: DecorationImage(
+                              //  image: AssetImage('assts/images/ads.jpg'))
+                            ),
+                          ),
+                          const Text(
+                            'مندي',
+                            style: TextStyle(fontFamily: 'Tajawal'),
+                          ),
+                        ],
+                      );
+                    })),
+            Padding(
+              padding: const EdgeInsets.only(left: 230.0),
+              child: const Text(
+                'المطاعم الاكثر زيارة',
+                style: OptionStyle,
+              ),
+            ),
+            SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: ListView.builder(
+                    //shrinkWrap: true,
+                    // physics:NeverScrollableScrollPhysics(),
+                    padding: const EdgeInsets.all(8),
+                    itemCount: 6,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              child: Image(
+                                fit: BoxFit.fill,
+                                image: AssetImage('assets/images/ress.jpg'),
+                              ),
+                            ),
+                            VerticalSpace(22),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 12.0),
+                              child: Text('data'),
+                            ),
+                            VerticalSpace(22),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 12.0),
+                              child: Icon(Icons.star),
+                            ),
+                          ],
+                        ),
+                      );
+                    })),
+          ]),
         ]),
       ),
     ));
