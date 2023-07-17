31d8f37307faa97678f22e6be378758387aac912
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-07-17 15:01:45 +0300

    fav screen edites

--- lib/ui/screens/favourite_screen.dart
+++ lib/ui/screens/favourite_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../../utils/constants.dart';
 
 class FavScreen extends StatefulWidget {
-  const FavScreen({Key? key}) : super(key: key);
+  FavScreen({Key? key}) : super(key: key);
 
   @override
   State<FavScreen> createState() => _FavScreenState();
@@ -16,15 +16,13 @@ class _FavScreenState extends State<FavScreen> {
       home: Scaffold(
           backgroundColor: AppColors.greyBackground,
           body: Padding(
-            padding: const EdgeInsets.only(top: 22.0),
+            padding: EdgeInsets.only(top: 40.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                const Padding(
-                  padding: EdgeInsets.only(left: 230.0),
-                  child: Text(
-                    'المطاعم الاكثر زيارة',
-                    style: HeadTextStyle,
-                  ),
+                Text(
+                  ' قائمة المفضلة ',
+                  style: HeadTextStyle,
                 ),
                 VerticalSpace(12),
                 Expanded(
@@ -33,8 +31,8 @@ class _FavScreenState extends State<FavScreen> {
                       height: MediaQuery.of(context).size.height,
                       width: double.infinity,
                       child: ListView.builder(
-                          padding: const EdgeInsets.all(8),
-                          itemCount: 6,
+                          padding: EdgeInsets.all(8),
+                          itemCount: images.length,
                           shrinkWrap: true,
                           itemBuilder: (BuildContext context, int index) {
                             return Card(
@@ -43,22 +41,34 @@ class _FavScreenState extends State<FavScreen> {
                                 mainAxisSize: MainAxisSize.min,
                                 children: <Widget>[
                                   Container(
+                                    // margin: const EdgeInsets.all(5),
+                                    height: 190,
                                     width: MediaQuery.of(context).size.width,
-                                    child: Image(
-                                      fit: BoxFit.fill,
-                                      image:
-                                          AssetImage('assets/images/ress.jpg'),
+                                    decoration: BoxDecoration(
+                                      color: Color.fromARGB(255, 202, 200, 200),
+                                      borderRadius: BorderRadius.circular(8),
+                                      image: DecorationImage(
+                                        image: NetworkImage(images[index]),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  VerticalSpace(12),
+                                  Padding(
+                                    padding: EdgeInsets.only(right: 12.0),
+                                    child: Text(
+                                      nameOfResturantsList[index],
+                                      style: HeadTextStyle,
                                     ),
                                   ),
                                   VerticalSpace(22),
                                   Padding(
-                                    padding: const EdgeInsets.only(right: 12.0),
-                                    child: Text('data'),
-                                  ),
-                                  VerticalSpace(22),
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 12.0),
-                                    child: Icon(Icons.star),
+                                    padding: EdgeInsets.only(
+                                        right: 12.0, bottom: 20),
+                                    child: Icon(
+                                      Icons.star,
+                                      color: Colors.yellow,
+                                    ),
                                   ),
                                 ],
                               ),
@@ -71,3 +81,20 @@ class _FavScreenState extends State<FavScreen> {
     );
   }
 }
+
+List images = [
+  'https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8N3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+  'https://images.pexels.com/photos/327158/pexels-photo-327158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8N3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+];
+List<String> nameOfResturantsList = [
+  'مطعم سويا',
+  'مطعم رويال كالاكسي ',
+  'مطعم سويا',
+  'مطعم رويال كالاكسي ',
+  'مطعم سويا',
+  'مطعم رويال كالاكسي ',
+  'مطعم بيت حلب'
+];
