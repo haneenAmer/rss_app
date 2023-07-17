d54e6239990044029eb6101948e9a336f3487a6a
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-07-17 15:01:53 +0300

    edites

---
+++ lib/ui/screens/food_menue_more.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FoodMenue extends StatelessWidget {
+  const FoodMenue({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
