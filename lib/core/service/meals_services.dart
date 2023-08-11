55314c24054760bb62ae4b78e8f31e1a7433db51
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-08-11 14:46:26 +0300

    edite

--- lib/core/service/meals_services.dart
+++ lib/core/service/meals_services.dart
@@ -2,15 +2,15 @@ import 'package:dio/dio.dart';
 
 class MealsServicies {
   final String endpoint;
+  final String id;
 
-  MealsServicies(this.endpoint);
+  MealsServicies(this.endpoint, this.id);
   Dio dio = Dio();
 
   Future mealsResponse() async {
     try {
-      final response = await dio.get(endpoint,
-          options: Options(
-              headers: {"tenantId": "00d3531e-126a-4397-abfd-ba473ac2255d"}));
+      final response =
+          await dio.get(endpoint, options: Options(headers: {"tenantId": id}));
       print("i am here ${response.data}");
       return response.data;
     } catch (e) {
