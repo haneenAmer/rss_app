3f29db25a68102c442808717656ef510c0de145c
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-08-24 13:20:32 +0300

    meals riverpod creat

---
+++ lib/core/revierpod/meals.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rrs_app/core/models/meals.dart';
+import 'package:rrs_app/core/revierpod/id_provider.dart';
+import 'package:rrs_app/core/repository/meals_repostory.dart';
+
+final mealsFutureProvider = FutureProvider.autoDispose((ref) {
+  Future<Meals> meals;
+  final MealsReposiory mealsRepository =
+      MealsReposiory(ref.watch(idProvider.notifier).state ?? '');
+
+  meals = mealsRepository.getMealsModel();
+  print("i am here 3");
+  return meals;
+});
