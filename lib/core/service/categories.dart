8a5b6ff169932523e818f8a296d6f323724639cf
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-08-24 13:21:05 +0300

    small edites

--- lib/core/service/categories.dart
+++ lib/core/service/categories.dart
@@ -2,13 +2,12 @@ import 'package:dio/dio.dart';
 
 class CategoriesServices {
   final String endPoint;
-
   CategoriesServices(this.endPoint);
-  Dio dio = Dio();
-
+  final Dio dio = Dio();
   Future categoriesResponse() async {
     try {
       final response = await dio.get(endPoint);
+      print(" HI FROM CATEGORIES ${response.data}");
       return response.data;
     } catch (e) {
       rethrow;
